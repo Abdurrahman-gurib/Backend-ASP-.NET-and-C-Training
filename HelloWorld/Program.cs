420802e5ecfc08182108da8fb6acbf9209b5d870
Abdurrahman Gurib <abdurrahman.gurib@sdworx.com>
2023-11-07 14:55:31 +0400

    new updates

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -17,17 +17,32 @@ class Program
          */
         WriteLine("Welcome to SDWorx training!");
         //DateTime dt;
-        long x = 10; // int is 4 bytes numeric type
-            int y = (int)x; // long is 8 byte numeric type, implicit type conversion 
+        //long x = 10; // int is 4 bytes numeric type
+        // int y = (int)x; // long is 8 byte numeric type, implicit type conversion 
+        
+        //STRINGS
+        string a = "         A fox jump into the lake               ";
+        WriteLine(a.Contains("fox"));
+        WriteLine(a.ToUpper());
+        WriteLine(a.Clone());
+        WriteLine(a.Trim()); //remove whitespace in the phase as seen the the phrase above.
+        WriteLine(a.Length);
+        String path = @"C:\\Users\\SD233484\\OneDrive - SD Worx\\Desktop\\BACKEND TRAINING\\HelloWorld"; // \t and \n are escape sequence characters
 
         //int a;//declaration
         //a = 35; // assigning value
 
-        string a = "35";
+        // string a = "35";
         //int m = Int32.Parse(a);
-        int m = Convert.ToInt32(a);
-        WriteLine(m.GetType());
- 
+        // int m = Convert.ToInt32(a);
+        //WriteLine(m.GetType());
+
+        //strings
+
+
+
+
+
 
     }
 }
