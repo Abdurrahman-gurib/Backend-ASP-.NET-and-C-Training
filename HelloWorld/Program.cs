6ec097bb59fd91804d8aea5aa869ebbac37eedce
Abdurrahman Gurib <abdurrahman.gurib@sdworx.com>
2023-11-17 10:04:56 +0400

    updates

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,41 +1,83 @@
-﻿using System;
+﻿/*using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace HelloWorld
 
-class Program
+    // structs
+
+    Employee employee = new Employee();
+employee.Id = 100;
+    employee.firstName = "Noor";
+     employee.lastName = "Gurib";
+     employee.city = "Mauritius";
+    employee.age = 23;
+    employee.salary = 10000.00M;
+    WriteLine(Employee.GetDetails());
 {
-    static void Main()
+
+    struct Employee
     {
-        // Sample array with elements
-        int[] array = { 10, 5, 20, 10, 30, 20, 40, 30, 50, 50 };
+        public int Id;
+        public string firstName;
+        public string lastName;
+        public int age;
+        public string city;
+        public decimal salary;
+        public string country;
 
-        // Call the method to find the largest three distinct elements
-        var largestThreeDistinctElements = FindLargestThreeDistinctElements(array);
-
-        // Print the result
-        Console.WriteLine("Largest three distinct elements:");
-        foreach (var element in largestThreeDistinctElements)
+        public string GetDetails()
         {
-            Console.WriteLine(element);
+            return ${ Id} - { firstName } { lastName}, is age - { age} years old, comes from { city} and earns { salary}
+            "
+        }
+
+    }
+
+}*/
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create an instance of the Employee struct
+            Employee employee = new Employee();
+
+            // Assign values to the struct fields
+            employee.Id = 100;
+            employee.firstName = "Noor";
+            employee.lastName = "Gurib";
+            employee.city = "Mauritius";
+            employee.age = 23;
+            employee.salary = 10000.00M;
+
+            // Call the GetDetails method and print the result
+            Console.WriteLine(employee.GetDetails());
         }
     }
 
-    // Method to find the largest three distinct elements
-    static int[] FindLargestThreeDistinctElements(int[] array)
+    // Define the Employee struct
+    struct Employee
     {
-        // Remove duplicates and order the array in descending order. largest number display first in the list
-        var distinctOrderedArray = array.Distinct().OrderByDescending(x => x).ToArray();
+        public int Id;
+        public string firstName;
+        public string lastName;
+        public int age;
+        public string city;
+        public decimal salary;
+        public string country; // Note: I added this field assuming it's intended to be part of the struct.
 
-        // Ensure there are at least three distinct elements
-        if (distinctOrderedArray.Length >= 3)
+        // Define the GetDetails method to return employee details as a formatted string
+        public string GetDetails()
         {
-            // Take the first three elements
-            return distinctOrderedArray.Take(3).ToArray();
-        }
-        else
-        {
-            // Handle the case when there are less than three distinct elements
-            Console.WriteLine("Array does not have at least three distinct elements.");
-            return distinctOrderedArray.ToArray();
+            // Note: Corrected the syntax of the interpolated string
+            return $"{Id} - {firstName} {lastName}, {age} years old, from {city}, earns {salary:C}";
+            //return name city age =salary
         }
     }
 }
